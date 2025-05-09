93b4cf5eb00af7e831c527ca5240c345f43fb449
thaihoang18 <thaihoangdao18@gmail.com>
2025-05-09 09:18:31 +0700

    update Voucher + VoucherList

--- src/main/java/sample/hustbookstore/models/Voucher.java
+++ src/main/java/sample/hustbookstore/models/Voucher.java
@@ -1,4 +1,36 @@
 package sample.hustbookstore.models;
 
+import java.util.Date;
+
+
 public class Voucher {
+    private int voucher_id;
+    private String code;
+    private int remaining;
+    private float discount;
+    private Date duration;
+
+    public Voucher(String code, int remaining, float discount, Date duration, int voucher_id) {
+        this.code = code;
+        this.remaining = remaining;
+        this.discount = discount;
+        this.duration = duration;
+        this.voucher_id = voucher_id;
+    }
+
+    public String getCode() {return code;}
+
+    public void setCode(String code) {this.code = code;}
+
+    public int getRemaining() {return remaining;}
+
+    public void setRemaining(int remaining) {this.remaining = remaining;}
+
+    public float getDiscount() {return discount;}
+
+    public void setDiscount(float discount) {this.discount = discount;}
+
+    public Date getDuration() {return (java.sql.Date) duration;}
+
+    public void setDuration(Date duration) {this.duration = duration;}
 }
