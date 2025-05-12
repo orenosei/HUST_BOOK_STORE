3466254bf2020af69af0931b07108d848bd6f5e0
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-12 15:01:46 +0700

    update cart UI and controller

--- src/main/java/sample/hustbookstore/controllers/user/UserCartController.java
+++ src/main/java/sample/hustbookstore/controllers/user/UserCartController.java
@@ -1,4 +1,63 @@
 package sample.hustbookstore.controllers.user;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import sample.hustbookstore.models.Cart;
+import sample.hustbookstore.models.CartItem;
+
+import static sample.hustbookstore.LaunchApplication.localCart;
+import static sample.hustbookstore.LaunchApplication.localUser;
+
 public class UserCartController {
+
+    @FXML
+    private Button applyVoucherBtn;
+
+    @FXML
+    private Text discountValue;
+
+    @FXML
+    private Text subTotalValue;
+
+    @FXML
+    private Text totalValue;
+
+    @FXML
+    private VBox vboxPane;
+
+    @FXML
+    private TextField voucherField;
+
+    public void display(){
+        ObservableList<CartItem> itemList = Cart.getCartItemList(localCart.getCartId());
+
+        vboxPane.getChildren().clear();
+
+        for(int i = 0; i < itemList.size(); i++){
+            try{
+                FXMLLoader load = new FXMLLoader();
+                load.setLocation(getClass().getResource("/sample/hustbookstore/user/user-cartItemCard-view.fxml"));
+                AnchorPane pane = load.load();
+                UserCartItemController itemC = load.getController();
+                itemC.setData(itemList.get(i));
+
+                vboxPane.getChildren().add(pane);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void initialize(){
+        display();
+    }
+
+
+
 }
--- src/main/java/sample/hustbookstore/models/Cart.java
+++ src/main/java/sample/hustbookstore/models/Cart.java
@@ -1,25 +1,28 @@
 package sample.hustbookstore.models;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static sample.hustbookstore.LaunchApplication.localUser;
+import static sample.hustbookstore.LaunchApplication.localCart;
 
 public class Cart {
     private static Connection connect;
 
-    int cart_id;
-    int user_id;
-    float total_cost;
-    float final_price;
+    private int cart_id;
+    private int user_id;
 
-    public Cart(int cart_id, int user_id, float total_cost, float final_price) {
+    public int getCartId() {
+        return cart_id;
+    }
+
+    public Cart(int cart_id, int user_id) {
         this.cart_id = cart_id;
         this.user_id = user_id;
-        this.total_cost = total_cost;
-        this.final_price = final_price;
     }
 
     public static void initialize() {
@@ -40,9 +43,7 @@ public class Cart {
             if (resultSet.next()) {
                 cart = new Cart(
                         resultSet.getInt("cart_id"),
-                        resultSet.getInt("user_id"),
-                        resultSet.getFloat("total_cost"),
-                        resultSet.getFloat("final_price")
+                        resultSet.getInt("user_id")
                 );
                 //System.console().printf("Cart found for user_id: %d %d %f %f\n", cart.cart_id, cart.user_id, cart.total_cost, cart.final_price);
                 return cart;
@@ -55,7 +56,82 @@ public class Cart {
         }
     }
 
+    public static boolean addProductToCart(String product_id, int quantity) {
+        String query = "INSERT INTO cart_item (cart_id, product_id, quantity) VALUES (?, ?, ?)";
 
+        try (PreparedStatement statement = connect.prepareStatement(query)) {
+            statement.setInt(1, localCart.cart_id);
+            statement.setString(2, product_id);
+            statement.setInt(3, quantity);
+            statement.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+
+        }
+    }
+
+    public static ObservableList<CartItem> getCartItemList(int cartId) {
+        ObservableList<CartItem> cartItemList = FXCollections.observableArrayList();
+        String query = "SELECT * FROM cart_item WHERE cart_id = ?";
+
+        try (PreparedStatement prepare = connect.prepareStatement(query)) {
+            prepare.setInt(1, cartId);
+
+            try (ResultSet result = prepare.executeQuery()) {
+                while (result.next()) {
+                    cartItemList.add(new CartItem(
+                            result.getString("product_id"),
+                            result.getInt("quantity"),
+                            false
+                    ));
+                }
+            }
+            System.out.println(cartId);
+            System.out.println("Item test:");
+            cartItemList.forEach(item -> {
+                System.out.println("CartItem: Product ID = " + item.getProductId() +
+                        ", Quantity = " + item.getQuantity() +
+                        ", Selected = " + item.isSelected());
+            });
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cartItemList;
+    }
+
+    public static boolean updateProductQuantity(String productId, int quantity) {
+        String query = "UPDATE cart_item SET quantity = ? WHERE cart_id = ? AND product_id = ?";
+
+        try (PreparedStatement statement = connect.prepareStatement(query)) {
+            statement.setInt(1, quantity);
+            statement.setInt(2, localCart.cart_id);
+            statement.setString(3, productId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean deleteProductFromCart(String productId) {
+        String query = "DELETE FROM cart_item WHERE cart_id = ? AND product_id = ?";
+
+        try (PreparedStatement statement = connect.prepareStatement(query)) {
+            statement.setInt(1, localCart.cart_id);
+            statement.setString(2, productId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 
 
