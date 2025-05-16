e67f2e042c4a2acce3b60958eb96fa4e07ee6aca
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-17 04:22:58 +0700

    getTrending

--- src/main/java/sample/hustbookstore/controllers/user/UserDashboardController.java
+++ src/main/java/sample/hustbookstore/controllers/user/UserDashboardController.java
@@ -1,4 +1,86 @@
 package sample.hustbookstore.controllers.user;
 
+
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import javafx.scene.layout.StackPane;
+
+import sample.hustbookstore.models.Book;
+import sample.hustbookstore.models.Cart;
+
+import java.io.IOException;
+import java.util.List;
+
 public class UserDashboardController {
+
+    @FXML
+    private AnchorPane recommendationPane;
+    @FXML
+    private Button trendingLeftBtn;
+    @FXML
+    private AnchorPane trendingPane;
+    @FXML
+    private Button trendingRightBtn;
+    @FXML
+    private StackPane trendingStackPane;
+    @FXML
+    private Button stopBtn;
+
+    private int currentIndex = 0;
+    private Timeline autoSlideTimeline;
+    private boolean isAutoSlideEnabled = true;
+    private TranslateTransition currentTransition;
+    private List<Book> trendingBooks;
+
+    @FXML
+    public void initialize() {
+        loadTrendingBooks();
+        initializeCards();
+    }
+
+    private void loadTrendingBooks() {
+        Cart cart = new Cart();
+        trendingBooks = cart.getTrendingBooks();
+        if (trendingBooks.size() < 5) {}
+    }
+
+    private void initializeCards() {
+        trendingStackPane.getChildren().clear();
+
+        try {
+            for (Book book : trendingBooks) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/hustbookstore/user/user-trendingCard.fxml"));
+                Node card = loader.load();
+
+                UserTrendingCardController cardController = loader.getController();
+                cardController.setData(book);
+
+                trendingStackPane.getChildren().add(card);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    private void handleTrendingLeftBtn() {
+
+    }
+
+    @FXML
+    private void handleTrendingRightBtn() {
+
+    }
+
+    @FXML
+    private void handleStopBtn() {
+
+    }
 }
---
+++ src/main/java/sample/hustbookstore/controllers/user/UserTrendingCardController.java
@@ -0,0 +1,66 @@
+package sample.hustbookstore.controllers.user;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import sample.hustbookstore.models.Book;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UserTrendingCardController {
+
+
+    @FXML
+    private TextField authorField;
+
+    @FXML
+    private TextArea descriptionField;
+
+    @FXML
+    private TextField genreField;
+
+    @FXML
+    private ImageView imageField;
+
+    @FXML
+    private TextField isbnField;
+
+    @FXML
+    private TextArea nameField;
+
+    @FXML
+    private Text priceField;
+
+
+    public void setData(Book book) {
+        authorField.setText(book.getAuthor());
+        descriptionField.setText(book.getDescription());
+        genreField.setText(book.getGenre());
+        isbnField.setText(book.getIsbn());
+        nameField.setText(book.getName());
+        priceField.setText(book.getSellPrice().toString());
+
+        String imagePath = book.getImage();
+        Image image = null;
+        try {
+            if (imagePath != null && !imagePath.isEmpty()) {
+                URL imageUrl = new URL(imagePath);
+                image = new Image(imageUrl.toExternalForm(), 1000, 1600, true, true);
+            } else {
+                image = new Image(getClass().getResource("/sample/hustbookstore/img/notfound.jpg").toExternalForm(), 1000, 1600, true, true);
+            }
+        } catch (MalformedURLException e) {
+            URL resourceUrl = getClass().getResource("/" + imagePath);
+            if (resourceUrl != null) {
+                image = new Image(resourceUrl.toExternalForm(), 1000, 1600, true, true);
+            } else {
+                image = new Image(getClass().getResource("/sample/hustbookstore/img/notfound.jpg").toExternalForm(), 1000, 1600, true, true);
+            }
+        }
+        imageField.setImage(image);
+    }
+}
